fdf0a16ce13bfe065b0639ffc8acde7e05bb127d
arisa2545 <arisa8533@gmail.com>
2022-08-22 03:44:17 +0700

    edit user

--- src/views/Users/ShowUsers.vue
+++ src/views/Users/ShowUsers.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref, onMounted} from "vue";
 import UserDetail from "../../components/UserComponents/EachUserDetail.vue";
+import EditUser from "../../components/UserComponents/EditUser.vue";
 
 const users = ref([]);
 const getUsers = async () => {
@@ -51,7 +52,15 @@ const closeShowDetail = () => {
   isShowDetail.value = 0
   isShowEdit.value = 0
 }
-
+const showEditForm = (user) => {
+  isShowEdit.value = 1
+  detailCurrentUser.value = user
+  console.log(isShowEdit.value)
+}
+const closeEditForm = () => {
+  isShowEdit.value = 0
+  console.log(isShowEdit.value)
+}
 </script>
 
 <template>
@@ -100,7 +109,7 @@ const closeShowDetail = () => {
               
               <div id="allButton" class="float-right col-6">
                 <img  @click="showDetail(user.id)" src="../../assets/detail.png" class="cursor-pointer img-button col-1">
-                <img  src="../../assets/edit.png" class="cursor-pointer img-button col-1"> 
+                <img  @click="showEditForm(user)" src="../../assets/edit.png" class="cursor-pointer img-button col-1"> 
                 <img  @click="deleteUsers(user.id)" src="../../assets/trash-bin.png" class="cursor-pointer img-button col-1">
               </div>
             </div>
@@ -126,7 +135,9 @@ const closeShowDetail = () => {
       </span>
     </div>
 
-    <UserDetail v-if="isShowDetail == true" @closeMe="closeShowDetail" :user="detailCurrentUser" />
+    <UserDetail v-if="isShowDetail == 1" @closeMe="closeShowDetail" :user="detailCurrentUser" />
+    <EditUser class="edit" v-if="isShowEdit == 1" @closeEditUser="closeEditForm" :users="users" :user="detailCurrentUser"
+      :showEditForm="isShowEdit" />
 
     </div>
   </main>
@@ -197,5 +208,9 @@ const closeShowDetail = () => {
 .scrollBar {
   overflow-y: scroll;
   height: 70vh;
+}
+
+.edit {
+  width: 100%;
 }
 </style>
